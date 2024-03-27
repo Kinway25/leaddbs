2e51a0e8fd2053ccb94d330e3cabf56d51726966
Konstantin Butenko <kbutenko@bwh.harvard.edu>
2024-03-27 22:23:08 +0100

    UPD: stn only logit regression

--- ea_logit_regression.m
+++ ea_logit_regression.m
@@ -64,16 +64,22 @@ for pt_j = 1:length(gpatsel)
         temp2=strsplit(obj.M.patient.list{gpatsel(pt_j)},'_');
         pt_ID2 = temp2{2}; 
         %trajDepth2 = temp2{end}(4:end);
-        side2 = temp2{3};
-        side_label = side2;
+        %side2 = temp2{3};
+        %side_label = side2;
         
         if contains(obj.M.patient.list{gpatsel(pt_j)}, '_fl.')
-            amp = str2num(temp2{end-1}(1:3));
-            trajDepth2 = temp2{end-1}(4:end);
+            %amp = str2num(temp2{end-1});
+            %trajDepth2 = temp2{end}(4:end);
+            side2 = 'left';
         else
-            amp = str2num(temp2{end}(1:3));
-            trajDepth2 = temp2{end}(4:end);
+            %amp = str2num(temp2{end});
+            %trajDepth2 = temp2{end}(4:end);
+            side2 = 'right';
         end
+
+        trajDepth2 = temp2{end}(4:end);
+        side_label = side2;
+        amp = str2num(temp2{end-1});
 
         row_to_add = {pt_ID2,trajDepth2,side_label,Ihat(pt_j),amp, Improvement(pt_j)};
         if ~exist('PatientsIntraOpTest_FF','var')
@@ -94,56 +100,56 @@ PatientsIntraOpTest_FF.Code = categorical(PatientsIntraOpTest_FF.Code);
 %PatientsIntraOpTest_FF.FF_score = zscore(PatientsIntraOpTest_FF.FF_score);
 % %writetable(PatientsIntraOpTest_FF,'/home/konstantin/Documents/Data/JR/JR_flipped/FF_Test_Table_V2_low_sigma.csv')
 % 
-%glme2 = fitglme(PatientsIntraOpTest_FF,'Response~FF_score+(1|Code)','Distribution','Binomial','Link','logit','FitMethod','Laplace');
+glme = fitglme(PatientsIntraOpTest_FF,'Response~FF_score+(1|Code)','Distribution','Binomial','Link','logit','FitMethod','Laplace');
 
 
 
-% let's predict post-op
+% % let's predict post-op
+% 
+% counter = 0;
+% for pt_j = 1:length(gpatsel)
+% 
+%     if test(pt_j) == 1
+% 
+%         counter = counter + 1;
+% 
+%         temp2=strsplit(obj.M.patient.list{gpatsel(pt_j)},'_');
+%         pt_ID2 = temp2{2}; 
+%         %trajDepth2 = temp2{end}(4:end);
+%         if contains(obj.M.patient.list{gpatsel(pt_j)},'left')
+%             side2 = 'left';
+%         else
+%             side2 = 'right';
+%         end
+%         %side2 = temp2{3};
+%         side_label = side2;
+%         
+%         amp = temp2{end-1};
+%         contact_N = temp2{end}(1);
+% 
+%         row_to_add = {pt_ID2,contact_N,side_label,Ihat(pt_j),amp};
+%         if ~exist('PatientsPostOpTest_FF','var')
+%             PatientsPostOpTest_FF = table;
+%             PatientsPostOpTest_FF.Code = pt_ID2;
+%             PatientsPostOpTest_FF.Contact = contact_N;
+%             PatientsPostOpTest_FF.Side = side_label;
+%             PatientsPostOpTest_FF.FF_score = Ihat(pt_j);
+%             PatientsPostOpTest_FF.Amp = amp;
+%             %PatientsPostOpTest_FF.Response = Improvement(pt_j);
+%         else
+%             PatientsPostOpTest_FF = [PatientsPostOpTest_FF;row_to_add];
+%         end
+%     end
+% end
+% 
+% PatientsPostOpTest_FF.Code = categorical(PatientsPostOpTest_FF.Code);
+% %PatientsPostOpTest_FF.FF_score = zscore(PatientsPostOpTest_FF.FF_score);
+% scores_test = predict(glme2,PatientsPostOpTest_FF);
+% Ihat_prediction = scores_test > 0.5913;
+% %Ihat_prediction = scores_test > 0.7023;
+% PatientsPostOpTest_FF.Response = Ihat_prediction;
 
-counter = 0;
-for pt_j = 1:length(gpatsel)
-
-    if test(pt_j) == 1
-
-        counter = counter + 1;
-
-        temp2=strsplit(obj.M.patient.list{gpatsel(pt_j)},'_');
-        pt_ID2 = temp2{2}; 
-        %trajDepth2 = temp2{end}(4:end);
-        if contains(obj.M.patient.list{gpatsel(pt_j)},'left')
-            side2 = 'left';
-        else
-            side2 = 'right';
-        end
-        %side2 = temp2{3};
-        side_label = side2;
-        
-        amp = temp2{end-1};
-        contact_N = temp2{end}(1);
-
-        row_to_add = {pt_ID2,contact_N,side_label,Ihat(pt_j),amp};
-        if ~exist('PatientsPostOpTest_FF','var')
-            PatientsPostOpTest_FF = table;
-            PatientsPostOpTest_FF.Code = pt_ID2;
-            PatientsPostOpTest_FF.Contact = contact_N;
-            PatientsPostOpTest_FF.Side = side_label;
-            PatientsPostOpTest_FF.FF_score = Ihat(pt_j);
-            PatientsPostOpTest_FF.Amp = amp;
-            %PatientsPostOpTest_FF.Response = Improvement(pt_j);
-        else
-            PatientsPostOpTest_FF = [PatientsPostOpTest_FF;row_to_add];
-        end
-    end
-end
-
-PatientsPostOpTest_FF.Code = categorical(PatientsPostOpTest_FF.Code);
-%PatientsPostOpTest_FF.FF_score = zscore(PatientsPostOpTest_FF.FF_score);
-scores_test = predict(glme2,PatientsPostOpTest_FF);
-Ihat_prediction = scores_test > 0.5913;
-%Ihat_prediction = scores_test > 0.7023;
-PatientsPostOpTest_FF.Response = Ihat_prediction;
-
-scores_test = predict(mdl,Ihat(test));
+%scores_test = predict(mdl,Ihat(test));
 
 
 % figure
@@ -205,9 +211,9 @@ scores_test = predict(mdl,Ihat(test));
 mdl = fitglm(Ihat(test),Improvement(test),'Distribution','binomial','Link','logit');
 
 % second, we run ROC curve analysis
-%scores = mdl.Fitted.Probability;
 %[X,Y,T,AUC,OPTROCPT] = perfcurve(Improvement(training),scores,1);
-scores = glme2.Fitted;
+%scores = glme.Fitted;
+scores = mdl.Fitted.Probability;
 [X,Y,T,AUC,OPTROCPT] = perfcurve(Improvement(test),scores,1);
 if disp_regression_plots
     figure, plot(X,Y, 'k', 'linew', 1.5)
@@ -264,8 +270,8 @@ end
 
 
 % prediction for test based on the logit model
-%scores_test = predict(mdl,Ihat(test));
-scores_test = predict(glme2,PatientsIntraOpTest_FF);
+scores_test = predict(mdl,Ihat(test));
+%scores_test = predict(glme,PatientsIntraOpTest_FF);
 
 
 Ihat_prediction = scores_test > scores_thresh;
@@ -289,323 +295,109 @@ if disp_regression_plots
 end
 
 
-% correlation plots for each patient
 
-% vals as contrast of two protocols
-%amp_patients = cell(1, 100);  % store amps for each patient
-%counter = 1;
-I_test = Improvement(test);
+% let's check prediction accuracy for each patient
+predicted_response = Ihat_prediction;
 
-pt_i = 1;
-difs = -1*ones(100,1);
-difs_mean = -1*ones(100,1);
-corr_vals_r = zeros(100,1);
-corr_vals_p = zeros(100,1);
-pt_N = 1;
-pt_K = 1;
+% let's find the actual amplitudes
+% I will need to re-add again from the original file checking for STN
+load('/home/konstantin/Documents/Data/JR/PatientsEMGOR_corr.mat')
 
-depths_max = -1*ones(39,1);
 
-lost_prediction = 0;
-total_predictions = 0;
-amps_not_predicted = [];
-all_predicted_amps = [];
+pt_counter = 0;
+pt_label = 'JohnDoe';
+difs = cell(42,1);
+difs_1mA = 0;
+difs_2mA = 0;
+difs_2_plus_mA = 0;
+difs_mean_1mA = 0;
+difs_mean_2mA = 0;
+difs_mean_2_plus_mA = 0;
+already_checked = [];
+for measure_i = 1:length(predicted_response)
 
-while pt_i <= length(gpatsel)-1
-    temp=strsplit(obj.M.patient.list{gpatsel(pt_i)},'_');
-    %disp(temp{end}(1:3))
+    % only check if over the threshold and response positive
+    if predicted_response(measure_i) == 1
 
-    % iterate only over that patients VTAs
-    pt_ID = temp{2};
-    %trajDepth = temp{end}(4:end);
-    %side = temp{3};
-
-    amps = [];
-    amps_predicted = [];
-
-    depths = [];  % - will be neglected
-
-    scores = [];
-    scores_predicted = [];
-    scores_av = [];
-
-    % check both sides
-    sides = {'right', 'left'};
-    for side_idx = 1:length(sides) 
-
-        true_resp = 0;
-
-        amp_checked = ["None"];
-        amp_pred_checked = ["None"];
-        amps_side = [];
-        amps_predicted_side = [];
-
-        side = sides{side_idx};
-        % check amplitude for side-effect
-        for pt_j = pt_i:length(gpatsel)
-            temp2=strsplit(obj.M.patient.list{gpatsel(pt_j)},'_');
-            pt_ID2 = temp2{2}; 
-            %trajDepth2 = temp2{end}(4:end);
-            side2 = temp2{3};
+        parts = strsplit(obj.M.patient.list{gpatsel(measure_i)},'_');
+        % stupid way to recover label and traj depth
+        trajDepth = parts{end}(4:end-4);
+        pt_checked = parts{2}(5:end);
+        amp = str2num(parts{end-1});
     
-            if contains(obj.M.patient.list{gpatsel(pt_j)}, '_fl.')
-                amp = str2num(temp2{end-1}(1:3));
-                trajDepth2 = temp2{end-1}(4:end);
-            else
-                amp = str2num(temp2{end}(1:3));
-                trajDepth2 = temp2{end}(4:end);
-            end
+        if contains(obj.M.patient.list{gpatsel(measure_i)}, 'right')
+            side_label = 'right';
+            hemi_label = 're';
+        else
+            side_label = 'left';
+            hemi_label = 'li';
+        end
     
-            % exit if different patient
-            if ~strcmp(pt_ID2, pt_ID)
-                break
-            elseif ~strcmp(side,side2)
-                continue
-            else
-
-                if I_test(pt_j) == 1 && ~any(contains(amp_checked, trajDepth2)) 
-
-                    if strcmp(pt_ID, 'MNI/SBNC46CB')
-                        disp("here")
-                    end
-
-%                     if amp > 6.0
-%                         amp_checked =  [amp_checked,"No response"];
-%                         depths = [depths, -1];
-%                         amps_side = [amps_side, NaN];
-%                     else
-
-    % 
-    %                     if amp == 0.5
-    %                         disp("____________-")
-    %                         disp(pt_ID2)
-    %                         disp(trajDepth2)
-    %                         disp(side2)
-    %                     end
+        traj = char(extract(trajDepth,lettersPattern));
+        depth = str2num(trajDepth(length(traj)+1:end));
     
-                        amps_side = [amps_side, amp];
-                        scores = [scores, scores_test(pt_j)];
-                        %disp(trajDepth2)
-                        if contains(trajDepth2, '.nii')
-                            depths = [depths, str2num(trajDepth2(end-6:end-4))];
-                        else
-                            depths = [depths, str2num(trajDepth2(end-2:end))];
-                        end
-    
-                        %disp(pt_j)
-                        %disp(depths(end))
-                        %disp("-----------")
-    
-                        amp_checked =  [amp_checked,trajDepth2];
-                        %found_amp = 1;
-                        true_resp = true_resp + 1;
-                    %end
-
-                elseif amp == 7.0 && I_test(pt_j) == 0 
-                    amps_side = [amps_side, NaN];
-                    depths = [depths, -1];
-                    amp_checked =  [amp_checked,"No response"];
-                end
-    
-                % also skip checking the same elecrode
-                if Ihat_prediction(pt_j) == 1 && ~any(contains(amp_pred_checked, trajDepth2))
-    
-                    amps_predicted_side = [amps_predicted_side, amp];  % predicted amps for response
-                    scores_predicted = [scores_predicted, scores_test(pt_j)];
-    
-                    amp_pred_checked =  [amp_pred_checked,trajDepth2];
-                    %found_pred_amp = 1;
-    
-                    %disp(trajDepth_current2)
-                elseif amp == 7.0 && Ihat_prediction(pt_j) == 0
-                    amps_predicted_side = [amps_predicted_side, NaN];
-                    amp_pred_checked =  [amp_pred_checked,"No prediction"];
-                end
-            end
+        % let's find clinical thresholds for this patient, trajectory and side
+        pt_entry = find(PatientsEMGOR4.Code == pt_checked & PatientsEMGOR4.Hemisphere == hemi_label & PatientsEMGOR4.Traj == traj & PatientsEMGOR4.Depth == depth);
+        
+        if ismember(pt_entry,already_checked)
+            %already checked this electrode
+            continue
+        else
+            already_checked = [already_checked,pt_entry];
         end
 
-        % I need to sort here, only matching pairs
-        amps_predicted_sorted_side = [];
-        amps_sorted_side = [];
-        for i = 2:length(amp_checked)
-            if ~isnan(amps_side(i-1))
-                for j = 2:length(amp_pred_checked)
-                    if ~isnan(amps_predicted_side(j-1)) && strcmp(amp_pred_checked(j), amp_checked(i))
-                        amps_sorted_side = [amps_sorted_side, amps_side(i-1)];
-                        amps_predicted_sorted_side = [amps_predicted_sorted_side, amps_predicted_side(j-1)];
-                    end
-                end
-            end
+        if length(pt_entry) > 1
+            disp(vta_i)
+        end
+    
+        if ~isinf(PatientsEMGOR4.ClinAny(pt_entry)) && ~isnan(PatientsEMGOR4.ClinAny(pt_entry))
+            amp_actual = PatientsEMGOR4.ClinAny(pt_entry);
         end
 
-        %lost_prediction = lost_prediction + (length(amps_side)-length(amps_sorted_side));
-        lost_prediction = lost_prediction + (true_resp-length(amps_sorted_side));
-        total_predictions = total_predictions + true_resp;
 
-         if strcmp(pt_ID, 'MNI/SBNC46CB')
-             disp("check")
-         end
-
-        amps = [amps,amps_sorted_side];
-        amps_predicted = [amps_predicted,amps_predicted_sorted_side];
+        if ~strcmp(pt_checked,pt_label)
+            % new patient
+            pt_label = pt_checked;
+            pt_counter = pt_counter + 1;
+            difs{pt_counter} = abs(amp_actual - amp);
+        else
+            difs{pt_counter} = [difs{pt_counter},abs(amp_actual - amp)];
+        end
     end
-
-
-%     amps_mean = round(mean(amps));
-% 
-%     % check scores at specific amplitude
-%     for pt_j = pt_i:length(gpatsel)
-%         temp2=strsplit(obj.M.patient.list{gpatsel(pt_j)},'_');
-%         pt_ID2 = temp2{2}; 
-%         if contains(obj.M.patient.list{gpatsel(pt_j)}, '_fl.')
-%             amp= str2num(temp2{end-1}(1:3));
-%         else
-%             amp = str2num(temp2{end}(1:3));
-%         end
-% 
-%         % exit if different patient
-%         if ~strcmp(pt_ID2, pt_ID)
-%             break
-%         else
-%             if amps_mean  == amp
-% 
-%                 %amps = [amps, amp];
-%                 scores_av = [scores_av, scores_test(pt_j)];
-%             end
-%         end
-%     end
-
-
-    % correlate within the patient
-    %figure
-    %ea_corrplot(amps,scores)
-    %ea_corrplot(amps,amps_predicted,nan, {pt_ID, 'Amp', 'Pred Amp (res 0.5)'})
-    %ea_corrplot(amps,scores_av)
-
-    if length(amps(~isnan(amps))) > 3 && length(amps_predicted(~isnan(amps_predicted))) > 3
-        [corr_vals_r(pt_N), corr_vals_p(pt_N)] = ea_permcorr(amps',amps_predicted','spearman');
-        %[difs(pt_N),idx_max] = ea_nanmax(abs(amps'-amps_predicted'));
-        %depths_max(pt_N) = depths(idx_max);
-        %difs_mean(pt_N) = ea_nanmean(abs(amps'-amps_predicted'));
-        %disp(pt_N)
-
-%         if length(amps_predicted(~isnan(amps_predicted))) > 5 && length(amps(~isnan(amps))) > 5
-%             figure
-%             ea_corrplot(amps,amps_predicted,nan, {pt_ID, 'Amp', 'Pred Amp (res 0.5)'})
-%         end
-
-        if corr_vals_r(pt_N) < 0.33
-            disp("here")
-        end
-
-        pt_N = pt_N + 1;
-    else
-        disp("skipped corr")
-    end
-
-    %disp(pt_ID)
-    if all(isempty(amps))
-        difs(pt_K) = [];
-        difs_mean(pt_K) = [];
-    else
-        [difs(pt_K),idx_max] = ea_nanmax(abs(amps'-amps_predicted'));
-        difs_mean(pt_K) = ea_nanmean(abs(amps'-amps_predicted'));
-
-        if difs_mean(pt_K) > 2
-            disp(pt_ID)
-            disp(amps)
-        end
-
-    end
-    pt_K = pt_K + 1;
-
-    pt_i = pt_j;
-
-    all_predicted_amps = [all_predicted_amps;amps_predicted'];
-
-%     % check only the response ones
-%     if Improvement(test(pt_i))  == 1
-% 
-%         if contains(obj.M.patient.list{gpatsel(pt_i)}, '_fl.')
-%             amp{counter} = str2num(temp{end-1}(1:3));
-%         else
-%             amp{counter} = str2num(temp{end}(1:3));
-%         end
-%     end
-
 end
 
-%disp("depths_max")
-%disp(depths_max)
+% you can check how many indices are missing in already_checked
 
-%figure
-%histogram(all_predicted_amps,[0,1,2,3,4,5,6,7,8,9])
-
-disp("Number of lost electrode predictions")
-disp(lost_prediction)
-disp("Out of ")
-disp(total_predictions)
-
-%figure()
-%histogram(depths_max,9)
-
-corr_vals_r(pt_N:end) = [];
-difs(pt_K:end) = [];
-difs_mean(pt_K:end) = [];
-
-% make bar plot of max errors and correlations
-small_r =  corr_vals_r(corr_vals_r<0.33);
-moderate_r =  corr_vals_r(corr_vals_r>0.33 & corr_vals_r<0.66);
-high_r =  corr_vals_r(corr_vals_r>0.66);
-
-X = categorical({'R<0.33', '0.33<R<0.66', 'R>0.66'});
-X = reordercats(X,{'R<0.33', '0.33<R<0.66', 'R>0.66'});
-Y = [length(small_r), length(moderate_r), length(high_r)];
-figure()
-bar(X,Y)
-title('Fiber Model: Spearman Predicted vs Actual Threshold')
-
-
-difs_1mA = difs(difs<=1);
-difs_2mA = difs(1<difs & difs<=2);
-difs_2_plus_mA = difs(2<difs);
-
-
+for pt_i = 1:42
+    if ~isempty(find(2<difs{pt_i}))
+        difs_2_plus_mA = difs_2_plus_mA + 1;
+    elseif ~isempty(find(1<difs{pt_i} & difs{pt_i}<=2))
+        difs_2mA = difs_2mA + 1;
+    else
+        difs_1mA = difs_1mA + 1;
+    end
+    if 2<mean(difs{pt_i})
+        difs_mean_2_plus_mA = difs_mean_2_plus_mA + 1;
+    elseif 1<mean(difs{pt_i}) && mean(difs{pt_i})<=2
+        difs_mean_2mA = difs_mean_2mA + 1;
+    else
+        difs_mean_1mA = difs_mean_1mA + 1;
+    end
+    % difs_1mA = difs_1mA + length(find(difs{pt_i}<=1));
+    % difs_2mA = difs_2mA + length(find(1<difs{pt_i} & difs{pt_i}<=2));
+    % difs_2_plus_mA = difs_2_plus_mA + length(find(2<difs{pt_i}));
+end
 X = categorical({'<=1 mA', '<=2 mA', '>2 mA'});
 X = reordercats(X,{'<=1 mA', '<=2 mA', '>2 mA'});
-Y = [length(difs_1mA), length(difs_2mA), length(difs_2_plus_mA)];
+Y = [difs_1mA, difs_2mA, difs_2_plus_mA];
 figure()
 bar(X,Y)
 title('Fiber Model: Max Patient Error')
-
-difs_mean_1mA = difs_mean(difs_mean<=1);
-difs_mean_2mA = difs_mean(1<difs_mean & difs_mean<=2);
-difs_mean_2_plus_mA = difs_mean(2<difs_mean);
-
-
 X = categorical({'<=1 mA', '<=2 mA', '>2 mA'});
 X = reordercats(X,{'<=1 mA', '<=2 mA', '>2 mA'});
-Y = [length(difs_mean_1mA), length(difs_mean_2mA), length(difs_mean_2_plus_mA)];
+Y = [difs_mean_1mA, difs_mean_2mA, difs_mean_2_plus_mA];
 figure()
 bar(X,Y)
 title('Fiber Model: Mean Patient Error')
-
-
-% % plot for Jan
-% X = categorical({'<=1 mA', '<=2 mA', '>2 mA'});
-% Y = [0, 6, 26 ;length(difs_1mA), length(difs_2mA), length(difs_2_plus_mA)];
-% figure()
-% bar(X,Y)
-% title('Fiber Model: Max in Patient Error')
-% legend({'Amplitude Model','Fiber Model'},...
-%     'Location','northwest')
-% 
-% X = categorical({'<=1 mA', '<=2 mA', '>2 mA'});
-% X = reordercats(X,{'<=1 mA', '<=2 mA', '>2 mA'});
-% Y = [9, 27, 6;length(difs_mean_1mA), length(difs_mean_2mA), length(difs_mean_2_plus_mA)];
-% figure()
-% bar(X,Y)
-% title('Fiber Model: Mean in Patient Error')
-% legend('Amplitude Model','Fiber Model')
 
 end
